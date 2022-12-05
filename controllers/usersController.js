d685d0c5245da698933caf6d66139a23bbc69152
ersankarimi <ersankarimii311@gmail.com>
2022-12-06 02:09:06 +0800

    feat: create insert user function

--- controllers/usersController.js
+++ controllers/usersController.js
@@ -8,4 +8,29 @@ const getAllUser = async (req, res) => {
   });
 };
 
-module.exports = { getAllUser };
+const insertUser = async (req, res) => {
+  const { nim, nama_lengkap, id_prodi, angkatan, jenis_kelamin } = req.body;
+  connection.query(
+    `CALL insert_data_pengguna('${nim}', '${nama_lengkap}', ${id_prodi}, ${angkatan}, '${jenis_kelamin}')`,
+    (err, rows) => {
+      if (err) {
+        console.error(err);
+        return res.status(400).json({
+          errors: {
+            success: false,
+            message: "Something went wrong",
+            code: err.code,
+          },
+        });
+      }
+
+      return res.json({
+        success: true,
+        description: rows,
+        data: { nim, nama_lengkap, id_prodi, angkatan, jenis_kelamin },
+      });
+    }
+  );
+};
+
+module.exports = { getAllUser, insertUser };
