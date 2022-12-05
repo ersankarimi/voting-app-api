543f6553deacd00f22756462b14df60e2ad2ad24
ersankarimi <ersankarimii311@gmail.com>
2022-12-05 23:55:04 +0800

    feat: import modules from dbConn.js
    Call the connectDB function to execute that function to connect to the database

--- server.js
+++ server.js
@@ -2,8 +2,16 @@ const express = require("express");
 require("dotenv").config();
 
 const app = express();
+const { connectDB, connection } = require("./config/dbConn");
 
 const PORT = process.env.PORT || 3000;
+
+// connect database
+connectDB();
+
+app.get("/", (req, res) => {
+  res.send("Homepage");
+});
 
 app.listen(PORT, () => {
   // eslint-disable-next-line no-console
