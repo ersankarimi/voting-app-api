ab1b7548468a9d0af1c1955cb1c8e8d2e53e7ded
ersankarimi <ersankarimii311@gmail.com>
2022-12-06 00:02:46 +0800

    feat: execute the logger and express.json() middleware

--- server.js
+++ server.js
@@ -1,13 +1,20 @@
 const express = require("express");
 require("dotenv").config();
+const { logger } = require("./middleware/logEvents");
 
 const app = express();
-const { connectDB, connection } = require("./config/dbConn");
+const { connectDB } = require("./config/dbConn");
 
 const PORT = process.env.PORT || 3000;
 
 // connect database
 connectDB();
+
+// custom middleware logger
+app.use(logger);
+
+// built-in middleware for json
+app.use(express.json());
 
 app.get("/", (req, res) => {
   res.send("Homepage");
