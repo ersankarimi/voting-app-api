1c5ec767169c8088ca3b6b5c15ba07709b37efe0
ersankarimi <ersankarimii311@gmail.com>
2022-12-06 09:03:03 +0800

    feat: create update user data function

--- controllers/usersController.js
+++ controllers/usersController.js
@@ -38,6 +38,7 @@ const insertUser = async (req, res) => {
 
       return res.json({
         success: true,
+        message: "Success insert the data",
         description: rows,
         data: { nim, nama_lengkap, id_prodi, angkatan, jenis_kelamin },
       });
@@ -45,4 +46,46 @@ const insertUser = async (req, res) => {
   );
 };
 
-module.exports = { getAllUser, insertUser };
+const updateUserData = (req, res) => {
+  const { nim, nama_lengkap, id_prodi, angkatan, jenis_kelamin } = req.body;
+
+  const updateData = () =>
+    connection.query(
+      `CALL update_data_pengguna(?,?,?,?,?);`,
+      [nim, nama_lengkap, id_prodi, angkatan, jenis_kelamin],
+      (err, rows) => {
+        if (err) {
+          return res.status(400).json({
+            errors: {
+              success: false,
+              message: "Something went wrong",
+              code: err.code,
+            },
+          });
+        }
+
+        return res.json({
+          success: true,
+          message: "Success update the data",
+          description: rows,
+          data: { nim, nama_lengkap, id_prodi, angkatan, jenis_kelamin },
+        });
+      }
+    );
+
+  connection.query(
+    `SELECT * FROM pengguna WHERE nim = ?`,
+    [req.params.nim],
+    (err, rows) => {
+      if (rows.length < 1 || err) {
+        return res.status(204).json({
+          success: false,
+        });
+      }
+
+      return updateData();
+    }
+  );
+};
+
+module.exports = { getAllUser, insertUser, updateUserData };
