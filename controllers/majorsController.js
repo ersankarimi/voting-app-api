8363a866d42e6d72da66250f98592ead99450dc8
ersankarimi <ersankarimii311@gmail.com>
2022-12-06 10:35:15 +0800

    feat: create majors controllers

--- controllers/majorsController.js
+++ controllers/majorsController.js
@@ -1,1 +1,28 @@
 const { connection } = require("../config/dbConn");
+
+const insertMajors = (req, res) => {
+  const { id_jurusan, nama_jurusan } = req.body;
+  connection.query(
+    `CALL insert_data_jurusan(?, ?)`,
+    [id_jurusan, nama_jurusan],
+    (err, rows) => {
+      console.log(err, rows);
+      if (err) {
+        return res.status(400).json({
+          errors: {
+            success: false,
+            message: "Something went wrong",
+            code: err.code,
+          },
+        });
+      }
+      return res.json({
+        success: true,
+        message: "Success insert the data",
+        desription: rows,
+      });
+    }
+  );
+};
+
+module.exports = { insertMajors };
